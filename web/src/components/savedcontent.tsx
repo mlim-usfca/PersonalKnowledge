eb974ec2ac62dbe9b842157de324f6d7dc1e091b
Mandy <mandylau0520@gmail.com>
2024-04-14 22:20:00 -0700

    update savedcontent

--- web/src/components/savedcontent.tsx
+++ web/src/components/savedcontent.tsx
@@ -4,7 +4,6 @@ import NewCategoryModal from "./newcategory";
 import { SavedLink, SavedCategory } from '@/app/interfaces';
 import { fetchSavedCategories, fetchSavedLinks } from '@/app/api';
 
-
 const SavedContent: React.FC = () => {
     // const [links, setLinks] = useState<SavedLink[]>([]);
     const [categories, setCategories] = useState<SavedCategory[]>([]);
@@ -42,42 +41,6 @@ const SavedContent: React.FC = () => {
                     <NewCategoryModal onClose={() => setIsNewCategoryModalOpen(false)} />
                 )}
             </div>
-            {/* <div style={{ marginTop: '30px' }}>
-                <div className="w-full flex justify-between items-center mb-4">                
-                    {categories.map((category: SavedCategory) => (
-                        <div key={category.id} className="bg-white p-4 shadow rounded-md w-full">
-                            <h3 className="text-lg font-medium">{category.name}</h3>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                                {category.links.map((link: SavedLink) => (
-                                    <div key={link.id} className="bg-white p-4 shadow rounded-md w-full">
-                                        <h3 className="text-lg font-medium">{link.title}</h3>
-                                        <a href={link.url} className="text-xs text-indigo-600">{link.url}</a>
-                                        <div className="flex flex-wrap justify-left items-center flex-wrap gap-1.5 mt-2">
-                                            {link.tags.map((tag: { id: string, emoji: string, name: string }) => (
-                                                <button
-                                                    key={tag.id}
-                                                    className="flex items-center px-2 py-0.5 rounded-full text-xs sm:text-sm text-gray-500 border border-gray-300 shadow-sm hover:bg-gray-100"
-                                                >
-                                                    {tag.emoji} {tag.name}
-                                                </button>
-                                            ))}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                            <button
-                                className="px-4 py-3 text-xl text-black rounded-full hover:bg-gray-200"
-                                style={{ backgroundColor: 'transparent', border: 'none', fontWeight: 'bold', fontSize: 'larger' }}
-                                onClick={() => setIsNewLinkModalOpen(true)}
-                            >+</button>
-                            {isNewLinkModalOpen && (
-                                <NewLinkModal onClose={() => setIsNewLinkModalOpen(false)} />
-                            )}
-                        </div>
-
-                    ))}
-                </div>
-            </div> */}
             <div style={{ marginTop: '30px' }}>
                 <div>                
                     {categories.map((category: SavedCategory) => (
